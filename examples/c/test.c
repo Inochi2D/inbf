c34af0d3633225b034e1ece3cc4ef7ce9f6833d3
LunaTheFoxgirl <cli-luna@protonmail.com>
2024-03-25 03:39:21 +0100

    Make C example nicer to read with asserts

--- examples/c/test.c
+++ examples/c/test.c
@@ -1,6 +1,7 @@
 #include "inbf.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 int main() {
 
@@ -8,35 +9,26 @@ int main() {
     inbf_value_t* compound = inbf_value_create_compound();
 
     // Add some values to it
-    int err = inbf_value_set_compoundv(compound, "a", inbf_value_create_f32(24.0f));
-    err = inbf_value_set_compoundv(compound, "b", inbf_value_create_string("uwu?"));
-    err = inbf_value_set_compoundv(compound, "c", inbf_value_create_string("awawa?"));
+    assert(inbf_value_set_compoundv(compound, "a", inbf_value_create_f32(24.0f)) == INBF_SUCCESS, inbf_error_get());
+    assert(inbf_value_set_compoundv(compound, "b", inbf_value_create_string("uwu?")) == INBF_SUCCESS, inbf_error_get());
+    assert(inbf_value_set_compoundv(compound, "c", inbf_value_create_string("awawa?")) == INBF_SUCCESS, inbf_error_get());
 
+    inbf_value_t* ftarget;
 
     // Get the float value out of the compound
     float v;
-    inbf_value_t* ftarget;
-    err = inbf_value_get_compoundv(compound, "a", &ftarget);
-    if (err == INBF_ERROR) printf("1: %s\n", inbf_error_get());
-    
-    err = inbf_value_get_f32(ftarget, &v);
-    if (err == INBF_ERROR) printf("2: %s\n", inbf_error_get());
+    assert(inbf_value_get_compoundv(compound, "a", &ftarget) == INBF_SUCCESS, inbf_error_get());
+    assert(inbf_value_get_f32(ftarget, &v) == INBF_SUCCESS, inbf_error_get());
 
     // Get the string value out of the compound
     const char *text;
-    err = inbf_value_get_compoundv(compound, "b", &ftarget);
-    if (err == INBF_ERROR) printf("3: %s\n", inbf_error_get());
-
-    err = inbf_value_get_string(ftarget, &text);
-    if (err == INBF_ERROR) printf("4: %s\n", inbf_error_get());
+    assert(inbf_value_get_compoundv(compound, "b", &ftarget) == INBF_SUCCESS, inbf_error_get());
+    assert(inbf_value_get_string(ftarget, &text) == INBF_SUCCESS, inbf_error_get());
 
     // Get the string value out of the compound
     const char *text2;
-    err = inbf_value_get_compoundv(compound, "c", &ftarget);
-    if (err == INBF_ERROR) printf("3: %s\n", inbf_error_get());
-
-    err = inbf_value_get_string(ftarget, &text2);
-    if (err == INBF_ERROR) printf("4: %s\n", inbf_error_get());
+    assert(inbf_value_get_compoundv(compound, "c", &ftarget) == INBF_SUCCESS, inbf_error_get());
+    assert(inbf_value_get_string(ftarget, &text2) == INBF_SUCCESS, inbf_error_get());
 
     printf("%s %f %s\n", text, v, text2);
 
